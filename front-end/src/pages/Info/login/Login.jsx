d1d3edee00144ab52c387402cee3d44ab07fd119
winA <winnaba61@gmail.com>
2024-07-17 19:54:36 +0900

    0717
    Login 오류 수정

--- front-end/src/pages/Info/login/Login.jsx
+++ front-end/src/pages/Info/login/Login.jsx
@@ -11,7 +11,6 @@ export const Login = () => {
     const REST_API_KEY = import.meta.env.VITE_APP_REST_API_KEY_KAKAO;
     const REDIRECT_URI = import.meta.env.VITE_APP_REDIRECT_URI_KAKAO;
     const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
-    const code = new URL(window.location.href).searchParams.get('code');
 
     const kakaoHandler = () => {
         window.open(KAKAO_AUTH_URI, '_self');

86b3dade4668ade2484099a11c6d1caa1713e0a2
winA <winnaba61@gmail.com>
2024-08-16 23:18:21 +0900

    :fire: mypagecheck삭제

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { Home } from './pages/main/home/Home';
 import { Explain } from './pages/preview/explain/Explain';
 import { Login } from './pages/info/login/Login';
 import { MyPage } from './pages/info/myPage/MyPage';
-import { MyPageCheck } from './pages/info/myPage/MyPageCheck';
 import { Result } from './pages/main/result/Result';
 import { StrategyMain } from './pages/main/strategy/StrategyMain';
 import { StrategyGolden } from './pages/main/strategy/StrategyGolden';
@@ -34,7 +33,6 @@ function App() {
                                 <Route path="/login/oauth2/code/kakao" element={<LoginHandler />} />
                                 <Route path="/stockinfo" element={<StockInfo />} />
                                 <Route path="/mypage" element={<MyPage />} />
-                                <Route path="/mypagecheck" element={<MyPageCheck />} />
                                 <Route path="/result/:id" element={<Result />} />
                                 <Route path="/strategy" element={<StrategyMain />} />
                                 <Route path="/strategy/golden" element={<StrategyGolden />} />

d479e2c2da02ca46e6a480a85284ea110380a20d
lmhyuck <49359991+lmhyuck@users.noreply.github.com>
2024-09-05 18:56:24 +0900

    프론트코드 전략2개 추가 동기화 완료

--- src/pages/main/strategy/StrategyMain.jsx
+++ src/pages/main/strategy/StrategyMain.jsx
@@ -54,6 +54,8 @@ export const StrategyMain = () => {
         { label: '골든/데드', value: 'strategy/golden' },
         { label: '볼린저밴드', value: 'strategy/bollinger' },
         { label: 'RSI, MFI, MACD 지표 이용', value: 'strategy/rsi' },
+        { label: '엔벨로프', value: 'strategy/env' },
+        { label: '윌리엄스%R', value: 'strategy/williams' },
     ];
 
     const handleChange = (e) => {
---
+++ src/pages/main/strategy/StrategyWilliams.jsx
@@ -0,0 +1,92 @@
+//Williams 지표 이용 전략 설정 페이지
+
+import React, { useContext, useState } from 'react';
+import styles from './strategy.module.css';
+import { ColorBtn } from '../../../components/button/colorBtn/ColorBtn';
+import { InputBox } from '../../../components/box/inputBox/InputBox';
+import { StrategyWDTO } from '../../../types/StrategyDTO';
+import { StrategyContext } from '../../../context/StrategyContext';
+import { useNavigate, useParams, useLocation } from 'react-router-dom';
+import axios from 'axios';
+
+export const StrategyWilliams = () => {
+    const { setStrategy5Data } = useContext(StrategyContext);
+    const [formData, setFormData] = useState({
+        williamsPeriod: 0,
+    });
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    // 현재 경로에서 숫자 부분 추출
+    const pathSegments = location.pathname.split('/');
+    const id = pathSegments[pathSegments.length - 1]; // 경로의 마지막 부분이 ID
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => {
+            const newFormData = { ...prevData, [name]: Number(value) };
+
+            if (name === 'williamsPeriod' && parseFloat(value) < 0) {
+                alert('입력값은 0보다 작을 수 없습니다.');
+                return prevData;
+            }
+
+            return newFormData;
+        });
+    };
+
+    const handleSubmit = async () => {
+        const SURL=import.meta.env.VITE_APP_URI;
+        const strategy5DTO = new StrategyWDTO(formData);
+        console.log(strategy5DTO);
+        setStrategy5Data(strategy5DTO);
+
+        try {
+            const token = localStorage.getItem('jwt'); // JWT 토큰 가져오기
+            const response = await axios.post(`http://${SURL}/strategy/williams`, strategy5DTO, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            });
+            console.log('Response:', response.data);
+        } catch (error) {
+            console.error('There was an error submitting the common strategy!', error);
+        }
+
+        if (formData.williamsPeriod) {
+            navigate(`/result/${id}`);
+        } else {
+            if (!formData.williamsPeriod) {
+                alert('Williams %R 데이터를 입력해주세요.(기본값은 14입니다.)');
+            }
+        }
+    };
+
+    const handlePrevClick = () => {
+        navigate('/strategy');
+    };
+
+    return (
+        <div className={styles.strategy}>
+            <div className={styles.title}>Williams %R 지표 이용 전략 설정 페이지</div>
+            <div className={styles.select}>
+                <div className={styles.subtitle}>빠른 이동 평균 기간</div>
+                <div className={styles.input}>
+                    <InputBox
+                        type="text"
+                        placeholder="Williams 기간을 입력하세요."
+                        name="williamsPeriod"
+                        value={formData.williamsPeriod}
+                        onChange={handleChange}
+                    />
+                </div>
+            </div>
+
+            <div className={styles.btnWrapper}>
+                <ColorBtn className={styles.btnPrev} text="< 이전" onClick={handlePrevClick}/>
+                <ColorBtn className={styles.btnNext} text="백테스트" onClick={handleSubmit}/>
+            </div>
+        </div>
+    );
+};

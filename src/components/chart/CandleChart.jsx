b52f8e7beccf24678f131a0a49a8ab5f04db52a1
winA <winnaba61@gmail.com>
2024-09-13 15:30:23 +0900

    #33 임시-8

--- src/components/chart/CandleChart.jsx
+++ src/components/chart/CandleChart.jsx
@@ -14,7 +14,7 @@ export const CandleChart = ({ title, chartData }) => {
     const minLowest = Math.min(...chartData.map((entry) => entry.low));
     const maxVolume = Math.max(...chartData.map((entry) => entry.volume));
 
-    const range = 1000;
+    const range = maxHighest * 0.1;
 
     // 데이터 포맷팅 (ApexCharts에서 사용하는 형식으로 변환)
     const candleData = chartData.map((entry) => ({
@@ -147,8 +147,8 @@ export const CandleChart = ({ title, chartData }) => {
         },
         yaxis: [
             {
-                min: minLowest, // 캔들 차트 y축 최소값
-                max: maxHighest, // 캔들 차트 y축 최대값
+                min: minLowest - range, // 캔들 차트 y축 최소값
+                max: maxHighest + range, // 캔들 차트 y축 최대값
                 show: false, // y축 표시
                 tooltip: {
                     enabled: true,
@@ -159,7 +159,7 @@ export const CandleChart = ({ title, chartData }) => {
             {
                 opposite: true,
                 show: false, // y축 표시
-                max: maxVolume, // 막대 차트 y축 최대값
+                max: maxVolume * 1000, // 막대 차트 y축 최대값
                 min: 0, // 막대 차트 y축 최소값
                 height: "20%", // 볼륨 차트의 높이 20%
                 offsetY: 80, // 볼륨 차트를 캔들스틱 차트 아래에 위치

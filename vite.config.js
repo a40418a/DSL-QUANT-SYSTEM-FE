539b02c423fe4e4ff2cd77b3bea7b0faaae7e5ee
winA <winnaba61@gmail.com>
2024-09-29 16:14:01 +0900

    오류 수정

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -11,12 +10,11 @@ export default defineConfig({
         rollupOptions: {
             output: {
                 manualChunks(id) {
-                    if (id.includes("node_modules")) {
-                        return id
-                            .toString()
-                            .split("node_modules/")[1]
-                            .split("/")[0]
-                            .toString();
+                    if (id.includes('node_modules')) {
+                        if (id.includes('apexcharts')) {
+                            return 'apexcharts';
+                        }
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
                     }
                 },
             },

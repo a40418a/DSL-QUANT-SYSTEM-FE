96491cc692315b2a2693caa798cd106ff5651bf9
winA <winnaba61@gmail.com>
2024-09-29 16:32:17 +0900

    임시-7

--- src/layouts/navigator/Navigator.jsx
+++ src/layouts/navigator/Navigator.jsx
@@ -10,7 +10,7 @@ const SURL = import.meta.env.VITE_APP_URI;
 export const Navigator = () => {
     const [activePage, setActivePage] = useState('');
     const [userName, setUserName] = useState('');
-    const { token, logout } = useContext(AuthContext); // AuthContext에서 token과 logout 가져오기
+    const { token, logout } = useContext(AuthContext);  // AuthContext에서 token과 logout 가져오기
     const location = useLocation();
     const navigate = useNavigate();
 
@@ -18,8 +18,7 @@ export const Navigator = () => {
         setActivePage(location.pathname);
 
         const fetchData = async () => {
-            if (token) {
-                // 토큰이 있을 때만 사용자 정보 요청
+            if (token) {  // 토큰이 있을 때만 사용자 정보 요청
                 try {
                     const response = await axios.get(`${SURL}/userinfo`, {
                         headers: {
@@ -33,64 +32,55 @@ export const Navigator = () => {
             }
         };
         fetchData();
-    }, [location, token]); // 토큰이 변경되거나 위치가 바뀔 때마다 다시 요청
+    }, [location, token]);  // 토큰이 변경되거나 위치가 바뀔 때마다 다시 요청
 
     const logoutHandler = async () => {
         try {
-            await axios.post(
-                `${SURL}/logout/`,
-                {},
-                {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                        'Content-Type': 'application/json',
-                    },
+            await axios.post(`${SURL}/logout`, {}, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                    'Content-Type': 'application/json',
                 },
-            );
+            });
 
-            logout(); // AuthContext에서 로그아웃 처리
+            logout();  // AuthContext에서 로그아웃 처리
             alert('로그아웃 완료');
-            navigate('/login/');
+            navigate('/login');
         } catch (error) {
             console.error('로그아웃 진행 도중에 오류가 발생했습니다', error);
         }
     };
 
-    if (location.pathname !== '/' && location.pathname !== '/login/') {
+    if (location.pathname !== '/' && location.pathname !== '/login') {
         return (
             <div className={styles.navigator}>
                 <div>
-                    <Link to="/home/" className={styles.title}>
+                    <Link to="/home" className={styles.title}>
                         DSL QUANT
                     </Link>
                 </div>
                 <ul className={styles.menuWrapper}>
                     <li
                         className={classNames(styles.menu, {
-                            [styles.active]:
-                                activePage === '/stocklist/' || activePage === '/stockinfo/',
+                            [styles.active]: activePage === '/stocklist' || activePage === '/stockinfo',
                         })}
-                        onClick={() => navigate('/stocklist/')}
+                        onClick={() => navigate('/stocklist')}
                     >
                         상세정보
                     </li>
                     <li
                         className={classNames(styles.menu, {
                             [styles.active]:
-                                activePage.startsWith('/strategy/') ||
-                                activePage.startsWith('/result/'),
+                                activePage === '/strategy' ||
+                                activePage.startsWith('/result'),
                         })}
-                        onClick={() => navigate('/strategy/')}
+                        onClick={() => navigate('/strategy')}
                     >
                         전략설정
                     </li>
                 </ul>
                 <div className={styles.account}>
-                    <Link
-                        to="/mypage/"
-                        onClick={() => navigate('/mypage/')}
-                        className={styles.accountName}
-                    >
+                    <Link to="/mypage" onClick={() => navigate('/mypage')} className={styles.accountName}>
                         <p>{userName ? userName : '이름'}</p> 님
                     </Link>
                     <Link to="/" onClick={logoutHandler} className={styles.accountLogout}>
--- src/pages/main/stockList/StockList.jsx
+++ src/pages/main/stockList/StockList.jsx
@@ -1,51 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import styles from './stockList.module.css';
 import { useNavigate } from 'react-router-dom';
-import {
-    DataGrid,
-    gridPageCountSelector,
-    gridPageSelector,
-    useGridApiContext,
-    useGridSelector,
-} from '@mui/x-data-grid';
+import { DataGrid } from '@mui/x-data-grid';
+import Paper from '@mui/material/Paper';
 import axios from 'axios';
 import { getStockListClosing } from '../../../utils/stocklistApi';
 import { Loading } from '../../../components/loading/Loading';
-import { Box } from '@mui/system';
-import Pagination from '@mui/material/Pagination';
-import PaginationItem from '@mui/material/PaginationItem';
-
-const CustomPagination = () => {
-    const apiRef = useGridApiContext();
-
-    if (!apiRef) {
-        return null;
-    }
-
-    const page = useGridSelector(apiRef, gridPageSelector);
-    const pageCount = useGridSelector(apiRef, gridPageCountSelector);
-
-    return (
-        <Pagination
-            color="primary"
-            variant="outlined"
-            shape="rounded"
-            page={page + 1}
-            count={pageCount}
-            renderItem={(props) => <PaginationItem {...props} disableRipple />}
-            onChange={(event, value) => apiRef.current.setPage(value - 1)}
-        />
-    );
-};
 
 export const StockList = () => {
     const navigate = useNavigate();
     const [stockData, setStockData] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [paginationModel, setPaginationModel] = useState({
-        page: 0,
-        pageSize: 20,
-    });
 
     useEffect(() => {
         const fetchStockData = async () => {
@@ -67,47 +32,32 @@ export const StockList = () => {
     };
 
     const columns = [
-        {
-            field: 'market',
-            headerName: '종목명',
-            flex: 2,
-            headerAlign: 'center',
-        },
+        { field: 'market', headerName: '종목명', flex: 2 },
         {
             field: 'closingPrice',
             headerName: '현재가',
             flex: 1,
-            type: 'number',
-            headerAlign: 'center',
-            renderCell: (params) => {
-                const value = Number(params.value);
-                const formattedValue = value ? new Intl.NumberFormat().format(value) : '-';
-                return <span>{formattedValue}</span>;
-            },
+            // valueFormatter: ({ value }) => {
+            //     const numericValue = Number(value); // 문자열을 숫자로 변환
+            //     console.log('numericValue:', numericValue);
+            //     console.log('value:', value);
+            //     return !isNaN(numericValue)
+            //         ? numericValue.toLocaleString(undefined, {
+            //               minimumFractionDigits: 2,
+            //               maximumFractionDigits: 2,
+            //           })
+            //         : '-';
+            // },
         },
         {
             field: 'fluctuatingRate',
             headerName: '등락률 (%)',
             flex: 1,
-            type: 'number',
-            headerAlign: 'center',
-            renderCell: (params) => {
-                const value = Number(params.value); // 값을 숫자로 변환
-                const color = value < 0 ? 'var(--down-color)' : 'var(--up-color)';
-                return <span style={{ color }}> {value ? (value * 100).toFixed(2) : '-'}</span>; // 기본값 처리
-            },
         },
         {
             field: 'tradingVolume',
             headerName: '거래량',
             flex: 1,
-            type: 'number',
-            headerAlign: 'center',
-            renderCell: (params) => {
-                const value = Number(params.value);
-                const formattedValue = value ? new Intl.NumberFormat().format(value) : '-';
-                return <span>{formattedValue}</span>;
-            },
         },
     ];
 
@@ -125,29 +75,17 @@ export const StockList = () => {
         <div className={styles.stockList}>
             <div className={styles.title}>코인 종목</div>
             <div className={styles.table}>
-                <Box
-                    sx={{
-                        width: '100%',
-                        '& .MuiDataGrid-columnHeader': {
-                            backgroundColor: 'var(--point-color-2) ',
-                            '& .MuiDataGrid-columnHeaderTitle': {
-                                color: 'var(--color-white) ',
-                            },
-                        },
-                    }}
-                >
+                <Paper>
                     <DataGrid
                         rows={rows}
                         columns={columns}
-                        paginationModel={paginationModel}
-                        onPaginationModelChange={setPaginationModel}
+                        initialState={{
+                            pagination: { paginationModel: { page: 0, pageSize: 20 } },
+                        }}
                         pageSizeOptions={[20, 50, 100]}
-                        slots={{ pagination: CustomPagination }}
-                        onRowClick={(params) => onClick(params.row.market)}
-                        disableColumnResize
-                        disableColumnReorder
+                        onRowClick={(params) => onClick(params.row.name)}
                     />
-                </Box>
+                </Paper>
             </div>
         </div>
     );

fdd5a3b230af9e035396f20abf8804db9925e11f
lmhyuck <49359991+lmhyuck@users.noreply.github.com>
2024-10-25 04:11:56 +0900

    복합 전략 등록
    밸런싱을 이용한 복합 전략 관련 사항

--- src/types/StrategyDTO.ts
+++ src/types/StrategyDTO.ts
@@ -73,3 +73,79 @@ export class StrategyWDTO {
         this.williamsPeriod = data.williamsPeriod;
     }
 }
+
+export class MultiStrategyDTO {
+    // 공통 전략 정보
+    userId: number;
+    second_strategy: string;
+
+    // 수익률 비율
+    profitVsRate: number; // 첫 전략 기준 2번째 전략과의 수익률 비율 결과
+    finalProfitRate: number; // 복합 최종 수익률
+
+    // Bollinger Bands 정보
+    moveAvg: number;
+
+    // Envelope 전략 정보
+    moving_up: number;
+    moving_down: number;
+    movingAveragePeriod: number;
+
+    // Golden/Dead Cross 전략 정보
+    fastMoveAvg: number;
+    slowMoveAvg: number;
+
+    // RSI 정보
+    rsiPeriod: number;
+
+    // Williams 전략 정보
+    williamsPeriod: number;
+
+    // 두 번째 전략 결과 정보
+    second_finalCash: number;
+    second_finalAsset: number;
+    second_finalBalance: number;
+    second_profit: number;
+    second_profitRate: number;
+    second_numberOfTrades: number;
+
+    constructor(data: {
+        userId: number;
+        second_strategy: string;
+        profitVsRate: number;
+        finalProfitRate: number;
+        moveAvg: number;
+        moving_up: number;
+        moving_down: number;
+        movingAveragePeriod: number;
+        fastMoveAvg: number;
+        slowMoveAvg: number;
+        rsiPeriod: number;
+        williamsPeriod: number;
+        second_finalCash: number;
+        second_finalAsset: number;
+        second_finalBalance: number;
+        second_profit: number;
+        second_profitRate: number;
+        second_numberOfTrades: number;
+    }) {
+        this.userId = data.userId;
+        this.second_strategy = data.second_strategy;
+        this.profitVsRate = data.profitVsRate;
+        this.finalProfitRate = data.finalProfitRate;
+        this.moveAvg = data.moveAvg;
+        this.moving_up = data.moving_up;
+        this.moving_down = data.moving_down;
+        this.movingAveragePeriod = data.movingAveragePeriod;
+        this.fastMoveAvg = data.fastMoveAvg;
+        this.slowMoveAvg = data.slowMoveAvg;
+        this.rsiPeriod = data.rsiPeriod;
+        this.williamsPeriod = data.williamsPeriod;
+        this.second_finalCash = data.second_finalCash;
+        this.second_finalAsset = data.second_finalAsset;
+        this.second_finalBalance = data.second_finalBalance;
+        this.second_profit = data.second_profit;
+        this.second_profitRate = data.second_profitRate;
+        this.second_numberOfTrades = data.second_numberOfTrades;
+    }
+}
